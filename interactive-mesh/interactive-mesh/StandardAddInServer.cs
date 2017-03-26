adea289380458b939a30af7c6a607996d55f9298
Alan Mcintyre <github@codereclaimers.com>
2017-03-26 07:45:10 -0400

    Cleanup of mesh example.

--- interactive-mesh/interactive-mesh/StandardAddInServer.cs
+++ interactive-mesh/interactive-mesh/StandardAddInServer.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows;
 using Inventor;
@@ -7,15 +8,9 @@ using Application = Inventor.Application;
 
 namespace interactive_mesh
 {
-    /// <summary>
-    /// This is the primary AddIn Server class that implements the ApplicationAddInServer interface
-    /// that all Inventor AddIns are required to implement. The communication between Inventor and
-    /// the AddIn is via the methods on this interface.
-    /// </summary>
     [GuidAttribute("0b3929a0-ba85-4215-83d8-844c130e6d0f")]
     public class StandardAddInServer : Inventor.ApplicationAddInServer
     {
-        // Inventor application object.
         private Application mApp;
         private ButtonDefinition mButton;
 
@@ -78,16 +73,10 @@ namespace interactive_mesh
                 mNormals[3 * t + 1] = 0;
                 mNormals[3 * t + 2] = 1;
             }
-
         }
 
         public void Activate(Inventor.ApplicationAddInSite addInSiteObject, bool firstTime)
         {
-            // This method is called by Inventor when it loads the addin.
-            // The AddInSiteObject provides access to the Inventor Application object.
-            // The FirstTime flag indicates if the addin is loaded for the first time.
-
-            // Initialize AddIn members.
             mApp = addInSiteObject.Application;
 
             // Create a button and connect it to the CreateMesh method.
@@ -105,14 +94,10 @@ namespace interactive_mesh
 
         public void Deactivate()
         {
-            // This method is called by Inventor when the AddIn is unloaded.
-            // The AddIn will be unloaded either manually by the user or
-            // when the Inventor session is terminated
-
-            // TODO: Add ApplicationAddInServer.Deactivate implementation
-
-            // Release objects.
             mApp = null;
+
+            mButton.OnExecute -= CreateMesh;
+            mButton = null;
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
@@ -136,6 +121,39 @@ namespace interactive_mesh
                 // TODO: Add ApplicationAddInServer.Automation getter implementation
                 return null;
             }
+        }
+
+        private Asset GetAsset(string partialName, out bool inActiveDocument)
+        {
+            inActiveDocument = false;
+            var upperPartialName = partialName.ToUpper();
+
+            // Look in the active document first.
+            var partDoc = mApp.ActiveDocument as PartDocument;
+            if (null != partDoc)
+            {
+                foreach (Asset asset in partDoc.AppearanceAssets)
+                {
+                    if (asset.DisplayName.ToUpper().Contains(upperPartialName))
+                    {
+                        inActiveDocument = true;
+                        return asset;
+                    }
+                }
+            }
+
+            foreach (AssetLibrary assetLib in mApp.AssetLibraries)
+            {
+                foreach (Asset asset in assetLib.AppearanceAssets)
+                {
+                    if (asset.DisplayName.ToUpper().Contains(upperPartialName))
+                    {
+                        return asset;
+                    }
+                }
+            }
+
+            return null;
         }
 
         public void CreateMesh(NameValueMap nv)
@@ -166,13 +184,10 @@ namespace interactive_mesh
             var setupTimeSeconds = (DateTime.Now - T0).TotalSeconds;
             T0 = DateTime.Now;
 
-
             var msg = string.Format("N = {0}, {1} triangles\n", N, mNumTriangles)
                       + string.Format("Inventor setup time: {0} sec\n", setupTimeSeconds);
 
             T0 = DateTime.Now;
-
-            //var transaction = mApp.TransactionManager.StartTransaction(mApp.ActiveDocument, "test mesh");
 
             try
             {
@@ -205,14 +220,19 @@ namespace interactive_mesh
                 triangles.NormalSet = gns;
                 triangles.NormalBinding = NormalBindingEnum.kPerItemNormals;
                 triangles.NormalIndexSet = gis;
-                //triangles.ColorSet = oGraphicsColorSet;
-                //triangles.ColorBinding = ColorBindingEnum.kPerVertexColors;
-                //triangles.ColorIndexSet = oGraphicsIndexSet;
 
                 var trianglesSeconds = (DateTime.Now - T0).TotalSeconds;
-                T0 = DateTime.Now;
 
-                //transaction.End();
+                bool inActiveDocument;
+                var brassAsset = GetAsset("Brass - Satin", out inActiveDocument);
+                if (null != brassAsset)
+                {
+                    if (!inActiveDocument)
+                    {
+                        brassAsset = brassAsset.CopyTo(doc);
+                    }
+                    mGraphicsNode.Appearance = brassAsset;
+                }
 
                 msg = msg + string.Format("GraphicsDataSetsCollection.Add time: {0} sec\n", dataSetsTimeSeconds)
                     + string.Format("Coordinate set creation time: {0} sec\n", coordSetSeconds)
@@ -225,17 +245,10 @@ namespace interactive_mesh
             {
                 msg += string.Format("Exception: {0}", e.ToString());
             }
-            finally
-            {
-                //transaction.Abort();
-            }
 
             mApp.ActiveView.Update();
 
             System.Windows.MessageBox.Show(msg);
-
-
-
         }
     }
 }
